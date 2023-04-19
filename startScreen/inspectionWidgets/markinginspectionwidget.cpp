c9e3785c1df9fe92ca8519af054e7fdb15e0883d
iona.dommel-prioux <iona.dommel-prioux@donecle.com>
2023-04-19 10:57:00 +0200

    📝 aazdaz

--- startScreen/inspectionWidgets/markinginspectionwidget.cpp
+++ startScreen/inspectionWidgets/markinginspectionwidget.cpp
@@ -4,19 +4,14 @@
 #include "ui_inspectionWidget.h"
 #include <QGroupBox>
 
-MarkingInspectionWidget::MarkingInspectionWidget(QWidget *parent)
-    : InspectionWidget(MarkingInspection, parent)
-{
-    generateStickersLayout();
-}
+MarkingInspectionWidget::MarkingInspectionWidget(QWidget *parent) : InspectionWidget(MarkingInspection, parent) { generateStickersLayout(); }
 
 void MarkingInspectionWidget::generateStickersLayout()
 {
     QGroupBox *infoBox = new QGroupBox();
     QHBoxLayout *sickersInfoLayout = new QHBoxLayout(infoBox);
 
-    m_stickersLabel =
-        new QLabel("Stickers progress: (Validated/Analysed)", this);
+    m_stickersLabel = new QLabel("Stickers progress: (Validated/Analysed)", this);
     m_stickersValue = new QLabel("Stickers progress: (?/?)", this);
 
     sickersInfoLayout->addWidget(m_stickersLabel);
@@ -29,9 +24,7 @@ void MarkingInspectionWidget::setData(const Data &data)
 {
     InspectionWidget::setData(data);
     InspectionData markingData = data.getInspectionData(MarkingInspection);
-    QString stickerString = QString("( %1 / %2 )")
-                                .arg(markingData.validatedObject)
-                                .arg(markingData.analyzedObjects);
+    QString stickerString = QString("( %1 / %2 )").arg(markingData.validatedObject).arg(markingData.analyzedObjects);
 
     m_stickersValue->setText(stickerString);
     qDebug() << "MarkingInspectionWidget::setData()";
