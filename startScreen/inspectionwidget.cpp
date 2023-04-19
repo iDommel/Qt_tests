c643f5efb04a6f5f80d452944768129d1e2afe41
iona.dommel-prioux <iona.dommel-prioux@donecle.com>
2023-04-19 10:54:23 +0200

    💡 ADDED: comment for a test

--- startScreen/inspectionwidget.cpp
+++ startScreen/inspectionwidget.cpp
@@ -40,6 +40,7 @@ void InspectionWidget::generateButtonLayout()
     ui->mainLayout->addWidget(buttonBox);
 }
 
+// Adding a comment
 InspectionWidget::InspectionWidget(InspectionType inspectionType,
                                    QWidget *parent)
     : QWidget(parent), ui(new Ui::InspectionWidget)
